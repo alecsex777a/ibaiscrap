0ca4da2be3f1d7344d061464034e8933ca339604
alecsex777a <121067257+alecsex777a@users.noreply.github.com>
2023-01-05 17:21:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ async def my_event_handler(m):
     print(f'{cc}|{mes}|{ano}|{cvv}')
     with open('cards.txt', 'a') as w:
         w.write(fullinfo + '\n')
-    await client.send_message(SEND_CHAT, text, file = 'logo.jpg')
+    await client.send_message(SEND_CHAT, text, file = 'logo.png')
     time.sleep(5)
 
 
